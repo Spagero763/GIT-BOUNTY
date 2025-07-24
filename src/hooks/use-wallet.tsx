fcda740491676298644ecf08b7bea6f90d086f21
afolabi ayomide <afolabiayomide870@gmail.com>
2025-07-24 03:27:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-wallet.tsx
+++ src/hooks/use-wallet.tsx
@@ -3,15 +3,12 @@
 import React from 'react';
 import { WagmiProvider as WagmiProviderBase, createConfig, http } from 'wagmi';
 import { baseSepolia } from 'wagmi/chains';
-import { injected, walletConnect } from 'wagmi/connectors';
-
-const projectId = '3b8b39b56adddec8357a5528255d6a2f'; // Replace with your WalletConnect Project ID
+import { injected } from 'wagmi/connectors';
 
 const config = createConfig({
   chains: [baseSepolia],
   connectors: [
     injected(),
-    walletConnect({ projectId }),
   ],
   transports: {
     [baseSepolia.id]: http(),
